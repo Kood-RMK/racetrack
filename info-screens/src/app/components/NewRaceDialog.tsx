30e09e71a6f817bc8db28223e859fd079ee8046a
Mimi <mimi.ladva@gmail.com>
2025-02-18 18:47:36 +0200

    added time selection, but WIP

--- info-screens/src/app/components/NewRaceDialog.tsx
+++ info-screens/src/app/components/NewRaceDialog.tsx
@@ -12,7 +12,11 @@ import {
     Input,
     Label,
     Modal,
-    TextField
+    TextField,
+    ComboBox,
+    ListBox,
+    ListBoxItem,
+    Popover
 } from 'react-aria-components';
 import Icon from '@mdi/react';
 import { 
@@ -29,7 +33,8 @@ export default function NewRaceDialog() {
 
     return (
         <div className="bg-gray-100 w-80 h-56 p-4 gap-4 flex flex-col font-cairo">
-            {/* Time Slot Tag */}
+            
+            {/* Time Tag */}
             <TagGroup selectionMode="multiple" className="w-48 h-5 gap-2 flex flex-shrink-0">
               <TagList className="flex gap-2">
                <Tag className="bg-blue-200 w-24 h-5 text-xs text-blue-800 font-medium rounded flex justify-center items-center gap-1 p-1 whitespace-nowrap">
@@ -39,37 +44,61 @@ export default function NewRaceDialog() {
               </TagList>
             </TagGroup>
         
-
+            {/* Dialog */}
             <DialogTrigger>
 
+                {/* Button that triggers Dialog */}
                 <Button className="flex flex-col items-center gap-2 p-2 mt-8">
                     <Icon path={mdiCheckerboardPlus} size={1.5}/>
                     Schedule Race
                 </Button>
 
                 <Modal className="flex justify-center items-center fixed inset-0 bg-black/50 font-cairo">
-                    <Dialog className="bg-white shadow-lg p-6 w-1/2">
+                    <Dialog className="bg-gray-100 shadow-lg p-5 w-1/2">
                         
                         {/* Close Button */}
                         <div className="flex justify-end">
                             <Button slot="close">
-                                <Icon path={mdiWindowClose} size={0.8} className="flex justify-end text-gray-500"></Icon>
+                                <Icon path={mdiWindowClose} size={0.8} className="flex justify-end text-gray-400"></Icon>
                             </Button>
                         </div>
 
-
+                        <div className="px-32">
                         {/* Heading  & Timeslot*/}
-                        <div className="flex justify-between items-center w-full py-5">
+                        <div className="flex justify-between items-center w-full py-5 font-cairo">
+
                             <Heading className="text-2xl font-semibold text-center flex justify-start">
                                 Race Details
                             </Heading>
 
-                            <TextField className="w-48">
-                                    <div className="relative flex items-center">
-                                        <Icon path={mdiMenuDown} size={0.8} className="absolute left-2 text-gray-500"/>
-                                        <Input className="border rounded pl-8 py-1 w-full" placeholder="Timeslot" />
-                                    </div>
-                                </TextField>
+                            <ComboBox className="relative w-48">
+
+                                <Button>
+                                <div className="realtive flex items-center">
+                                  <Icon path={mdiClockTimeFour} size={0.9} className="absolute left-2 text-gray-500 pointer-events-none"></Icon>
+                                  <Input placeholder="Timeslot" className="border rounded pl-8 pr-8 py-1 w-full"/>
+                                  <Icon path={mdiMenuDown} size={1} className="absolute right-2 text-black pointer-events-none"></Icon>
+                                </div>
+                                </Button>
+
+                                <Popover className="bg-white/80 rounded w-48 p-2 shadow-lg backdrop-blur-md">
+                                  <ListBox className="max-h-48 overflow-y-auto font-cairo">
+                                    <ListBoxItem>9:00 - 9.30</ListBoxItem>
+                                    <ListBoxItem>9:30 - 10.00</ListBoxItem>
+                                    <ListBoxItem>10:00 - 10:30</ListBoxItem>
+                                    <ListBoxItem>10:30 - 11:00</ListBoxItem>
+                                    <ListBoxItem>11:00 - 11:30</ListBoxItem>
+                                    <ListBoxItem>11:30 - 12:00</ListBoxItem>
+                                    <ListBoxItem>12:00 - 12:30</ListBoxItem>
+                                    <ListBoxItem>12:30 - 13:00</ListBoxItem>
+                                    <ListBoxItem>13:30 - 14:00</ListBoxItem>
+                                    <ListBoxItem>14:30 - 15:00</ListBoxItem>
+                                    <ListBoxItem>15:30 - 16:00</ListBoxItem>
+                                  </ListBox>
+                                </Popover>
+
+                            </ComboBox>
+
                         </div>
                        
                         {/* Racecar Inputs */}
@@ -116,6 +145,7 @@ export default function NewRaceDialog() {
                         <p className="text-center text-sm text-gray-500 flex justify-start">Enter a number between 1 and 10</p>
                         </div>
 
+                      </div>
                     </Dialog>
                 </Modal>
             </DialogTrigger>
