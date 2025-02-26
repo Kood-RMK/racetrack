8f41c1fd11ec728c41c7a165b8c4263a0231b048
Kati Kraavi <katikraavi@gmail.com>
2025-02-26 18:17:08 +0200

    test

--- info-screens/src/app/components/AnnouncementHeader.tsx
+++ info-screens/src/app/components/AnnouncementHeader.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import React from "react";
-import "./Header.css";
+import {
+  Cell,
+  Column,
+  Row,
+  Table,
+  TableBody,
+  TableHeader
+} from 'react-aria-components';
 
-export const Header = ({ AnnouncementHeader, ...props }) => {
+export const Header = ({ className = "", AnnouncementHeader, ...props }) => {
   return (
-    <div className={"header " + className}>
+    <div className={`header ${className}`} {...props}>
       <div className="middle">
-        <div className="title">ANNOUNCEMENT </div>
+        <div className="title">ANNOUNCEMENT</div>
         <div className="line-1"></div>
-        <div className="subtext">Beachside Racetrack </div>
+        <div className="subtext">Beachside Racetrack</div>
       </div>
     </div>
   );
 };
+
