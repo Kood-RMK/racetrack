1bfc8e493cc1c702c3400b94345313be2dddaa02
Mimi Veesaar <mimi.ladva@gmail.com>
2025-03-05 23:22:35 +0200

    Race Control page

--- info-screens/src/app/admin/race-control/page.tsx
+++ info-screens/src/app/admin/race-control/page.tsx
@@ -1,10 +1,29 @@
-import type { ReactElement } from 'react'
+import RaceCard from '@/components/card/timeslot-card'
+import {  RacePendingButtons, ActiveRaceButtons, ActiveRaceHazardButtons, ActiveRaceDangerButtons, RaceCancelledButtons, EndSessionButtons } from '@/components/card/timeslot-card/Buttons'
+import { ActiveRaceDangerTags, ActiveRaceHazardTags, ActiveRaceTags, EndSessionTags, RaceCancelledTags, RacePendingTags } from '@/components/card/timeslot-card/Tags'
  
 
 export default async function Page() {
  
   return (
-    <p className='font-cairo text-silver'>Hello</p>
-  )
+    <div className='font-cairo flex flex-col min-h-full p-10 text-5xl'>
+      
+      <p className='text-center pt-14 mr-12'>Race Control</p>
 
+      <div>
+        <div className='grid grid-cols-3 w-full max-w-[calc(100vw-16rem)] mx-auto pl-10 pt-10 gap-10  items-start'>
+
+        <RaceCard tags={<RacePendingTags/>} buttons={<RacePendingButtons/>} />
+        <RaceCard tags={<ActiveRaceTags/>} buttons={<ActiveRaceButtons/>} />
+        <RaceCard tags={<ActiveRaceHazardTags/>} buttons={<ActiveRaceHazardButtons/>} />
+
+        <RaceCard tags={<ActiveRaceDangerTags/>} buttons={<ActiveRaceDangerButtons/>} />
+        <RaceCard tags={<RaceCancelledTags/>} buttons={<RaceCancelledButtons/>} />
+        <RaceCard tags={<EndSessionTags/>} buttons={<EndSessionButtons/>} />
+        
+
+        </div>
+      </div>
+    </div>
+  )
 }
