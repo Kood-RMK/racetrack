6af792d629952979c8faf89c3f95d29cf55370d7
Mimi Veesaar <mimi.ladva@gmail.com>
2025-03-05 23:33:29 +0200

    Race leaderboard added padding

--- info-screens/src/app/display/race-leaderboard/page.tsx
+++ info-screens/src/app/display/race-leaderboard/page.tsx
@@ -4,7 +4,7 @@ import LeaderboardTable from '@/components/table/leaderboard';
 
 export default function RaceTimerPage() {
   return (
-    <div className="bg-black min-h-screen min-w-screen text-white flex flex-col items-center">
+    <div className="bg-black min-h-screen min-w-screen text-white flex flex-col items-center pt-10">
       <Header />
       <LeaderboardTable />
     </div>
